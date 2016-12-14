2bef0c454b13201fa2daa36e65bf079dd24bfe82
derp01 <chris.nava.104@my.csun.edu>
2016-12-14 12:55:34 -0800

    Update infra.tf

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -182,11 +182,11 @@ resource "aws_security_group" "bastion" {
 		protocol = "tcp"
 		cidr_blocks = ["0.0.0.0/0"]
 	}
-  egress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
+  	egress {
+     	 	from_port = 0
+     		to_port = 0
+     	 	protocol = "-1"
+      	 	cidr_blocks = ["0.0.0.0/0"]
+  	}
 	vpc_id = "${var.vpc_id}"
 }
